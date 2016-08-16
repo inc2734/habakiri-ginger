12e17f71328b3590505dd09a2c6d43cb07008a61
Takashi Kitajima <inc@2inc.org>
2016-08-16 09:39:09 +0900

    Fixed a bug Habakiri_Plugin_GitHub_Updater

--- habakiri-ginger.php
+++ habakiri-ginger.php
@@ -3,11 +3,11 @@
  * Plugin Name: Ginger - Habakiri design skin
  * Plugin URI: https://github.com/inc2734/habakiri-ginger
  * Description: Ginger is a design skin of Habakiri. This plugin needs Habakiri 2.0.0 or later.
- * Version: 2.0.1
+ * Version: 2.1.0
  * Author: Takashi Kitajima
  * Author URI: http://2inc.org
  * Created : July 5, 2015
- * Modified: October 13, 2015
+ * Modified: August 16, 2016
  * Text Domain: habakiri-ginger
  * Domain Path: /languages/
  * License: GPLv2 or later
@@ -21,7 +21,10 @@ if ( defined( 'HABAKIRI_DESIGN_SKIN' ) && get_template() === 'habakiri' ) {
 define( 'HABAKIRI_DESIGN_SKIN', true );
 
 include_once( plugin_dir_path( __FILE__ ) . 'classes/class.config.php' );
-include_once( plugin_dir_path( __FILE__ ) . 'classes/class.github-updater.php' );
+
+if ( ! class_exists( 'Habakiri_Plugin_GitHub_Updater' ) ) {
+	include_once( plugin_dir_path( __FILE__ ) . 'classes/class.github-updater.php' );
+}
 new Habakiri_Plugin_GitHub_Updater( 'habakiri-ginger', __FILE__, 'inc2734' );
 
 class Habakiri_Ginger {
